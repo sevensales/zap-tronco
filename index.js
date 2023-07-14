cf11bb3a4edd8933882d300009f8d2508a09e0bd
José Carlos Marquetti Junior <iwasthesword@users.noreply.github.com>
2023-07-14 16:12:52 -0300

    check for message 2

--- index.js
+++ index.js
@@ -184,9 +184,12 @@ if (appAPIMessage) {
   });
 
   app.get("/message", (req, res) => {
-    const number = req.query.number;
-    const message = req.query.message.split("\\n").join("\n");
-    const fileUrl = req.query.fileUrl;
+    var number = req.query.number;
+    var message = req.query.message;
+    var fileUrl = req.query.fileUrl;
+    if (message) {
+      message = message.split("\\n").join("\n");
+    }
 
     if (number && (fileUrl || message)) {
       const numberWithSuffix =
