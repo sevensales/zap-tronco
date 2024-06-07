afe510ffd287ba43ce9e3dbf0a3901249e4fe348
José Carlos Marquetti Junior <iwasthesword@users.noreply.github.com>
2024-06-07 13:52:31 -0300

    nope

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const wwebVersion = "2.3000.1014043554-alpha";
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: false,
+        headless: true,
         args: ["--no-sandbox"],
     },
     webVersionCache: {
